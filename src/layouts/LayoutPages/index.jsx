f21836f59043d5f5033a485121e71787eef61699
ilhamSkyOf19 <ilhamm778866@gmail.com>
2025-06-22 00:51:43 +0700

    membuat navbar menjadi fragmen navbar usert

--- src/layouts/LayoutPages/index.jsx
+++ src/layouts/LayoutPages/index.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react'
 import ButtonNavigasi from '../../components/NavbarList';
 import IconArab from '../../components/IconArab';
-import { FaBarsStaggered, FaXmark } from 'react-icons/fa6';
 import useWindowSize from '../../hook/UseWindowSize';
 import PusatBantuan from '../../fragments/PusatBantuan';
 import { FaFacebookF, FaInstagram, FaTiktok, FaYoutube } from 'react-icons/fa6';
-import { Link, useLocation } from 'react-router-dom';
-import IconNavbar from '../../components/IconNavbar';
+import { Link } from 'react-router-dom';
+import Navbar from '../../fragments/NavbarUser';
 const index = ({ children }) => {
     // ====================
     // State
@@ -97,7 +96,7 @@ const index = ({ children }) => {
 
 
 
-
+    const [admin, setAdmin] = useState(true)
 
     return (
         <div className='flex flex-col overflow-hidden '>
@@ -110,86 +109,9 @@ const index = ({ children }) => {
 }
 
 
-// ===================
-// Navbar
-// ===================
 
 
-const Navbar = ({ showNavbar, handleClick, ref, toggleButtonRef, showNavbarList, widthDevice }) => {
 
-
-    const navbar = [
-        {
-            label: 'Home',
-            url: ''
-        },
-        {
-            label: 'Profile',
-            url: 'profile'
-        },
-        {
-            label: `Kema'hadan`,
-            url: 'kemahadan'
-        },
-        {
-            label: 'PSB',
-            url: 'psb'
-        },
-        {
-            label: 'Formulir',
-            url: 'formulir'
-        },
-    ]
-
-    // ==================
-    // Use Location 
-    // ==================
-    const location = useLocation();
-    const currentPath = location.pathname;
-
-
-    return (
-        <div ref={ref} className={`flex flex-row justify-between items-center z-50 bg-secondary-blue w-full h-18 fixed transition duration-350 ease-in-out ${showNavbar ? 'translate-y-0' : '-translate-y-full'} px-4 md:h-18 md:px-8 lg:h-14`}>
-            {widthDevice < 400 && <IconArab width={90} />}
-            {widthDevice > 700 && widthDevice <= 1024 && <IconArab width={100} />}
-            {widthDevice > 1024 && <IconNavbar />}
-
-            {widthDevice < 700 && <button
-                ref={toggleButtonRef}
-                onClick={handleClick}
-                className='flex flex-row justify-end items-center '>
-                <div className="relative w-8 h-8 p-2">
-                    <FaBarsStaggered
-                        className={`absolute inset-0 text-2xl transform transition-all duration-300 text-white cursor-pointer -scale-x-100 ${showNavbarList ? 'opacity-0 rotate-40 scale-75' : 'opacity-100 rotate-0 scale-100'
-                            } md:md:text-4xl`}
-                    />
-                    <FaXmark
-                        className={`absolute inset-0 text-2xl transform transition-all duration-300 text-white cursor-pointer ${showNavbarList ? 'opacity-100 scale-100' : 'opacity-0 scale-50'
-                            } md:text-4xl`}
-                    />
-                </div>
-            </button>}
-            {widthDevice > 700 && widthDevice <= 1024 &&
-                <div className='flex flex-row justify-end items-center gap-9'>
-                    <NavbarLinkTab link='' text='Home' />
-                    <NavbarLinkTab link='profile' text='Profile' />
-                    <NavbarLinkTab link='kemahadan' text={`Kema'hadan`} />
-                    <NavbarLinkTab link='psb' text='PSB' />
-                    <NavbarLinkTab link='formulir' text='Formulir' />
-                </div>
-            }
-            {widthDevice > 1024 &&
-                <div className='flex flex-row justify-end items-center gap-6'>
-                    {navbar.map((item, index) => (
-                        <NavbarLinkTabDesktop key={index} link={item.url} text={item.label} active={currentPath === `/${item.url}`} />
-                    ))}
-
-                </div>
-            }
-
-        </div>
-    )
-}
 
 // =================
 // Footer
